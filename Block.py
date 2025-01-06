09f91e2ba13ec447f1caac33dda97ad462f4389e
HelistormerAVD <kenneth.kulbach@gmail.com>
2025-01-06 20:54:09 +0100

    view verbessert und Block

--- Block.py
+++ Block.py
@@ -1,4 +1,6 @@
-
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import json
 
 class Block:
 
@@ -8,23 +10,26 @@ class Block:
     BLOCK_POS_Y = 0
     BLOCK_WIDTH = 20
     BLOCK_HEIGHT = 20
+    canvas = tk.Canvas()
 
     #LOKAL VARIABLES
     B_layout = {}
     B_color = None
     B_inputTypes = {}
     B_outputTypes = {}
-    
 
-    def __init__(self):
+
+    def __init__(self, tkCanvas):
         self.BLOCK_ID = 0
+        self.initBlock_add(tkCanvas)
 
-    def initBlock_add(self):
+    def initBlock_add(self, tkCanvas):
         self.BLOCK_ID = 1
         self.BLOCK_POS_X = 10
         self.BLOCK_POS_Y = 10
         self.BLOCK_WIDTH = 20
         self.BLOCK_HEIGHT = 20
+        self.canvas = tkCanvas
 
 
 
