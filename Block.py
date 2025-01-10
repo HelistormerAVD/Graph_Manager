e638841d2c622e90475fe7e2dcc671365875a82d
Helistormer <80815616+Helistormer@users.noreply.github.com>
2025-01-10 17:38:13 +0100

    # versuch ein gemeinsames system zu finden

--- Block.py
+++ Block.py
@@ -2,15 +2,13 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import json
 
+import BlockComponents
+import dataTypes
+
 
 class Block:
     # FIXED VARIABLES
-    BLOCK_ID = 0
-    BLOCK_POS_X = 0
-    BLOCK_POS_Y = 0
-    BLOCK_WIDTH = 20
-    BLOCK_HEIGHT = 20
-    canvas = tk.Canvas()
+
 
     blocks = {"B_type" : {}, "B_components" : {}, "B_position" : {}}
 
@@ -18,17 +16,14 @@ class Block:
     #"B_components" : {"id" : 0, "component_id" = 1, "c_position" : {}, "data" : dataTypes}
     #"B_position" : {"x1": 50, "y1": 50, "x2": 150, "y2": 150, "color": "blue"}
 
-    def __init__(self, tkCanvas):
-        self.BLOCK_ID = 0
-        self.initBlock_add(tkCanvas)
+    def __init__(self):
+        self.blocks[0] = {"B_type" : {"id" : 0, "block_id" : 1, "block_inputTypes" : {"input_id" : 0, "input_t" : dataTypes, "inputBlockId" : None}, "block_outputTypes" : {"output_id" : 0, "output_t" : dataTypes, "outputBlockId" : None}, "func" : {"func_name" : "NameOfFunction"}},
+                          "B_components" : {"id" : 0, "component" : BlockComponents},
+                          "B_position" : {"x1": 50, "y1": 50, "x2": 150, "y2": 150, "color": "blue"}}
 
-    def initBlock_add(self, tkCanvas):
-        self.BLOCK_ID = 1
-        self.BLOCK_POS_X = 10
-        self.BLOCK_POS_Y = 10
-        self.BLOCK_WIDTH = 20
-        self.BLOCK_HEIGHT = 20
-        self.canvas = tkCanvas
+    def initBlock_add(self):
+        length = self.blocks.__len__()
+
 
 
 if __name__ == "__main__":
---
+++ TODO.md
@@ -0,0 +1,19 @@
+# TODO:
+### - Block Klasse erstellen/verwendbar machen.
+    - generelle Blockfunktionen hinzufügen, wie (z.B. getPosition, getComponents, ...)
+    - Einzelne Blockarten erstellen (initialisieren wie bei "__init__")
+### - BlockComponents funktionsfähig machen.
+    - generelle Funktionen hinzufügen, wie (getPosition, changePosition, ...)
+### - Funktionen für DataTypes schreiben.
+    - Image.py in dataTypes.py übertragen/anpassen.
+    - Text.py in dataTypes.py (class BDString) übertragen/anpassen.
+    - Graph.py in dataTypes.py übertragen/anpassen.
+### - view mit den Objekten (wie Block.py) verknüpfen, bzw. verwenden
+    - Snapping von den Blöcken beheben
+    - verlinkung fixen
+### - Block Klasse erstellen/verwendbar machen.
+    - Einzelne Blockarten erstellen (initialisieren wie bei "__init__")
+### - BlockComponents verbessern (funktionen ergänzen wie überprüfung der Eingabe, z.B. ob nur Nummern erlaubt sind.)
+### - FileIO an die Blöcke etc. Anpassen 
+#### - Funktionen Abgabe Konform machen
+#### - Funktionen richtig Kommentieren
