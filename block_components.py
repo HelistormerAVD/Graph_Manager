b8d012ac4facfb62607e6a82bbac01942de482bb
Marshila <victoria.schwabe03@gmail.com>
2025-01-13 10:06:04 +0100

    Update block_components.py

--- block_components.py
+++ block_components.py
@@ -2,88 +2,58 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import json
 
-
 # Just a normal Text to be displayed
 class TextView:
+    text = "textView"
+    fontColor = "gray"
+    fontSize = 10
+    width = 20
+    height = 14
 
-    def __init__(self):
-        self.component_Id = 0
-        self.x1 = 0
-        self.y1 = 0
-        self.text = "TextView"
-        self.fontSize = 10
-        self.fontColor = "black"
-        self.width = 20
-        self.height = 14
+    def __init__(self, text):
+        self.text = text
 
 
     def getData(self):
-        return {"id" : self.component_Id,
-                "x1" : self.x1,
-                "y1" : self.y1,
-                "text" : self.text,
-                "fontSize" : self.fontSize,
-                "fontColor" : self.fontColor,
-                "width" : self.width,
-                "height" : self.height}
-
-    def setData(self, component_id : int, x1 : int, y1 : int, text : str, fontSize : int, fontColor : str, width : int, height : int):
-        self.component_Id = component_id
-        self.x1 = x1
-        self.y1 = y1
-        self.text = text
-        self.fontSize = fontSize
-        self.fontColor = fontColor
-        self.width = width
-        self.height = height
+        return {
+            "text": self.text,
+            "fontColor": self.fontColor,
+            "fontSize": self.fontSize,
+            "width": self.width,
+            "height": self.height
+        }
 
     def getText(self):
         return self.text
 
     def setText(self, txt):
         self.text = txt
-
-    def setPosition(self, posX, posY):
-        self.x1 = posX
-        self.y1 = posY
-
-    def getPosition(self):
-        return self.x1, self.y1
 
 
 # Editable Text Box
 class EditText:
+    text = "EditText"
+    fontColor = "black"
+    fontSize = 10
+    width = 20
+    height = 14
     isOnlyNumber = False
+    x = 0
+    y = 0
 
-    def __init__(self):
-        self.component_Id = 0
-        self.x1 = 0
-        self.y1 = 0
-        self.text = "EditText"
-        self.fontSize = 10
-        self.fontColor = "black"
-        self.width = 20
-        self.height = 14
-
-    def setData(self, component_id : int, x1 : int, y1 : int, text : str, fontSize : int, fontColor : str, width : int, height : int):
-        self.component_Id = component_id
-        self.x1 = x1
-        self.y1 = y1
+    def __init__(self, text):
         self.text = text
-        self.fontSize = fontSize
-        self.fontColor = fontColor
-        self.width = width
-        self.height = height
 
     def getData(self):
-        return {"id": self.component_Id,
-                "x1": self.x1,
-                "y1": self.y1,
-                "text": self.text,
-                "fontSize": self.fontSize,
-                "fontColor": self.fontColor,
-                "width": self.width,
-                "height": self.height}
+        return {
+            "text": self.text,
+            "fontColor": self.fontColor,
+            "fontSize": self.fontSize,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
 
     def getText(self):
         return self.text
@@ -91,20 +61,17 @@ class EditText:
     def setText(self, txt):
         self.text = txt
 
-    def setPosition(self, posX, posY):
-        self.x1 = posX
-        self.y1 = posY
-
     def getPosition(self):
-        return self.x1, self.y1
+        return self.x, self.y
 
-    def checkIfOnlyNumber(self):
-        checked = str.isdigit(self.text)
-        if checked:
-            return 1
-        else:
-            return 0
-
+    def changePosition(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
 
 if __name__ == "__main__":
     tx = TextView("Hello World!")
+    et = EditText("Editable")
+    print(tx.getData())
+    print(et.getData())
+    et.changePosition(100, 200)
+    print("New Position:", et.getPosition())
